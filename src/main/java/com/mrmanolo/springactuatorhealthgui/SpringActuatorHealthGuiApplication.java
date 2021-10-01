a2a842f0357d1929adce91a4fd1023f1f3550453
MrManolo <manueldiazgil96@hotmail.com>
2021-10-01 23:30:59 +0200

    added: base project

---
+++ src/main/java/com/mrmanolo/springactuatorhealthgui/SpringActuatorHealthGuiApplication.java
@@ -0,0 +1,13 @@
+package com.mrmanolo.springactuatorhealthgui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorHealthGuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuatorHealthGuiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrmanolo/springactuatorhealthgui/SpringActuatorHealthGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrmanolo.springactuatorhealthgui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorHealthGuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
